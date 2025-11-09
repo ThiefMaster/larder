cb61220a1c7e171bd34aa3f18d8a0ab91137b68e
Adrian Moennich <adrian@planetcoding.net>
2025-11-09 17:52:17 +0100

    Allow cascading item deletions

--- migrations/2025-09-16-202829-0000_create_stock/up.sql
+++ migrations/2025-09-16-202829-0000_create_stock/up.sql
@@ -1,6 +1,6 @@
 create table stock (
     id serial primary key,
-    item_id int not null references items(id),
+    item_id int not null references items(id) on delete cascade,
     added_dt timestamptz not null default now(),
     opened_dt timestamptz,
     removed_dt timestamptz
